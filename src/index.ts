9588a911578c73f078b5899470f3662236ddc369
Epen Flow <53342177+epenflow@users.noreply.github.com>
2024-07-27 18:36:07 +0800

    implement body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import route from './routes/api';
-
+import bodyParser from 'body-parser';
 dotenv.config();
 /** express */
 const app = express();
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT;
 
 /** express */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', route.export());
+
+/** express listener */
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
 });
