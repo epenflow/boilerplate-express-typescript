5065d2fc82770904f847fafddd94d85547caf310
epenflow <epenflow@gmail.com>
2024-07-27 13:35:24 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello world');
+});
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
